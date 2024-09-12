405c01fc785d8444f0295dfb99f7a537c046be09
Jason <mat.lopez072023@gmail.com>
2024-09-13 03:48:02 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get(`${api}/products`,async function(req, res) {
+    const productList = await Product.find({});
+
+    if(!productList) {
+        res.status(500).json({
+            succes: false
+        })
+    }
+    res.send(productList);
+})
+
+app.post(`${api}/products`, function(req, res) {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+    product.save().then((cratedProduct) => {
+        res.status(201).json(cratedProduct);
+    }).catch((err) => {
+        res.status(500).json({
+            error: err,
+            succes: false
+        });
+    })
+})
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshopDB"
+})
+.then(() => console.log("database connection successful, connection ready..."))
+.catch((err) => console.log(err));
+
+app.listen(3000, () => {
+    console.log(api);
+    console.log("server is running on http://localhost:3000");
+})
